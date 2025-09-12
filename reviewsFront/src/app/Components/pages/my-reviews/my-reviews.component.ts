e45a172b9a4d5b9cffd4dc208f186f555164496d
alejandro <ale.marcho0514@gmail.com>
2025-09-11 20:13:06 -0500

    Add components & import material angular

---
+++ reviewsFront/src/app/Components/pages/my-reviews/my-reviews.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-reviews',
+  imports: [],
+  templateUrl: './my-reviews.component.html',
+  styleUrl: './my-reviews.component.scss'
+})
+export class MyReviewsComponent {
+
+}
