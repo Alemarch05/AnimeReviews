27ec03c94c4e27a28e9a7b5a933ad4b2e32c4f71
alejandro <ale.marcho0514@gmail.com>
2025-09-30 16:26:39 -0500

    Feat: Dialog Anime

--- reviewsFront/src/app/Components/dialog/anime-dialog/anime-dialog.component.ts
+++ reviewsFront/src/app/Components/dialog/anime-dialog/anime-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Inject } from '@angular/core';
+import { Anime } from '../../../interfaces/anime';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-anime-dialog',
@@ -7,6 +11,12 @@ import { Component } from '@angular/core';
   styleUrl: './anime-dialog.component.scss'
 })
 export class AnimeDialogComponent {
-  
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Anime,
+   private dialogRef: MatDialogRef<AnimeDialogComponent>
+   ) {}
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
--- reviewsFront/src/app/shared/card/card.component.ts
+++ reviewsFront/src/app/shared/card/card.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Anime } from '../../interfaces/anime';
 import { Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarRatingPipe } from '../star-rating.pipe';
+import { MatDialog} from '@angular/material/dialog';
+import { AnimeDialogComponent } from '../../Components/dialog/anime-dialog/anime-dialog.component';
 
 @Component({
   selector: 'app-card',
@@ -13,5 +15,14 @@ import { StarRatingPipe } from '../star-rating.pipe';
 })
 export class CardComponent {
    @Input() animes: Anime[] = [];
+   constructor(private dialog : MatDialog) {}
+
+   openDialog(anime: Anime) {
+    this.dialog.open(AnimeDialogComponent, {
+      width: '600px',
+      height: '500px',
+      data: anime
+    });
+   }
 
 }
